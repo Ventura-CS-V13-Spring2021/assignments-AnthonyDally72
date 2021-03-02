94aebd1ea3f79e49704c3e8b26f03498c8bfb917
Anthony Dally <Anthony.Dally72@outlook.com>
2021-03-02 23:14:03 +0000

    Assignment 2-5 start

---
+++ Assignment2-5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+/*
+  Make a program that finds the prime numbers in the given range
+  Find the prime numbers in the list 1,100
+  Input two positive integers
+  output the list of prime number list in the given range 
+  Program logic
+  Input statement for the user inputs
+  make the input validation statement for the range
+  make the for-loop for the iteration with the user's range
+  make the for-loop for checking whether the number is a prime number or not
+  print out the number if it is prime*/
+int main()
+{
+  int pick_one, pick_two ,i;
+
+  cout << "enter two positive numbers : ";
+  cin >> pick_one >> pick_two ;
+  
+  if(pick_one <= 0)
+  {
+    cout << "Try again";
+
+  }
+  for(pick_one = i;i <= pick_two; i++)
+  {
+    if(pick_one / 2)
+    {
+      
+
+    }
+    cout << "This number is prime : ";
+
+  }
+  return 0;
+
+
+
+}
