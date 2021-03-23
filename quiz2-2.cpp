1096686a9a4286cc9d599c58dcd45cbb0d025f6a
Anthony Dally <Anthony.Dally72@outlook.com>
2021-03-23 02:48:25 +0000

    update to problem 1

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+
 using namespace std;
 
 // Make a program that generates 5 random numbers which are from 0 to 99 then find the number that is greater than the previous number
 
 int main()
 {
-    int a,
+   
 
-    random()
+    for (int i = 0; i <= 4; i++)
+    {
+      cout << "Enter a number between 0 and 99";
+
+
+
+    }
+
+    
+
+    
+
+
+   
+
+  return 0;
+
+    
 
 
 }
