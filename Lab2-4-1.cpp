32990165399b3670716169adf6db3c7e1a9e98e1
Anthony Dally <Anthony.Dally72@outlook.com>
2021-03-23 04:02:14 +0000

    got as far as I could with lab and quiz. I'll do better.

--- Lab2-4-1.cpp
+++ Lab2-4-1.cpp
@@ -4,19 +4,34 @@
 #include <ctime>
 using namespace std;
 
-// use filestream for writing
+/* use filestream for writing
+ Make a program that writes the given number N of integers to a file
+ The integers that we write to a file will be determined with a random number generation
+ The number of integers N is determined by the user input
+ The generated N random numbers are stored into a file.*/
 
 int main()
 {
 
   int random;
   int N;
-  ofstream    rdfile;
+  
+  ofstream    ofs;
 
-  rdfile.open("random.text");
+  ofs.open("random.text");
 
-  cout << "Enter the number for random \n";
+  cout << "Enter the number of random numbers to be added \n";
   cin >> N;
+
+  for (int i = 1; i <= N; i++)
+  {
+        
+        ofs << 1 + rand() % 100; 
+        cout << random;
+        
+
+
+  }
 
   
   
