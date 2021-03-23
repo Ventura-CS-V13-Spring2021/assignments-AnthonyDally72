25a48f4de589c5b90ca41a1b998169d3eeeea879
Anthony Dally <Anthony.Dally72@outlook.com>
2021-03-23 02:53:45 +0000

    switched to problem 3

--- quiz2-2.cpp
+++ quiz2-2.cpp
@@ -2,29 +2,11 @@
 
 using namespace std;
 
-// Make a program that generates 5 random numbers which are from 0 to 99 then find the number that is greater than the previous number
+// Make a program that read up the exact 3 integer values and check whether there are duplicated numbers or not.
 
 int main()
 {
    
-
-    for (int i = 0; i <= 4; i++)
-    {
-      cout << "Enter a number between 0 and 99";
-
-
-
-    }
-
-    
-
-    
-
-
-   
-
-  return 0;
-
     
 
 
